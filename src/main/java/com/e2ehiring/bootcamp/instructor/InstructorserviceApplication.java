d70a5c0945b625fac0e5ede0ac929fe11d4ca462
aakrati.singhal <aakrati.singhal@diatoz.com>
2023-03-01 12:00:50 +0530

    first checkin

---
+++ src/main/java/com/e2ehiring/bootcamp/instructor/InstructorserviceApplication.java
@@ -0,0 +1,13 @@
+package com.e2ehiring.bootcamp.instructor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstructorserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstructorserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/e2ehiring/bootcamp/instructor/InstructorserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e2ehiring.bootcamp.instructor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstructorserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
